d7142c7328495b5ea61a8321ca08e67d76fde35e
Dimas Bangkit S <dimasbangkits@gmail.com>
2021-04-03 16:57:40 +0700

    update

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -25,59 +25,6 @@ const config: MenuEntry[] = [
     icon: 'FarmIcon',
     href: 'https://cubdefi.com/farms',
   },
-  {
-    label: 'Dens',
-    icon: 'PoolIcon',
-    href: 'https://cubdefi.com/dens',
-  },
-  {
-    label: 'LeoBridge',
-    icon: 'NftIcon',
-    href: "https://bridge.cubdefi.com",
-  },
-  {
-    label: 'Info',
-    icon: 'InfoIcon',
-    items: [
-      {
-        label: 'GitHub',
-        href: 'https://github.com/CubFinance',
-      },
-      {
-        label: 'PancakeSwap',
-        href: 'https://pancakeswap.info/token/0x50d809c74e0b8e49e7b4c65bb3109abe3ff4c1c1',
-      },
-      {
-        label: 'CoinMarketCap',
-        href: 'https://coinmarketcap.com/currencies/cub-finance/',
-      },
-    ],
-  },
-  {
-    label: 'Docs',
-    icon: 'TicketIcon',
-    href: 'https://docs.cubdefi.com/',
-  },
-  {
-    label: 'Roadmap',
-    icon: 'MoreIcon',
-    href: 'https://docs.cubdefi.com/roadmap',
-  },
-  {
-    label: "Tokenized Blogging",
-    icon: "PawIcon",
-    href: "https://leofinance.io/",
-  },
-  {
-    label: "Blog",
-    icon: "BlogIcon",
-    href: "https://leofinance.io/@leofinance",
-  },
-  {
-    label: 'CertiK Audit (Coming Soon)',
-    icon: 'AuditIcon',
-    href: '#',
-  },
 ]
 
 export default config
